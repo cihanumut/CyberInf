a52d1eab05e7e58165c0f0f69a6f5461eb82d097
Cihan <164879616+cihanumut@users.noreply.github.com>
2026-03-12 18:58:51 +0300

    Update upload.js
    fix cloud

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const multer = require('multer');
-const { v2: cloudinary } = require('cloudinary');
+const cloudinary = require('cloudinary'); 
 const pkg = require('multer-storage-cloudinary');
 const CloudinaryStorage = pkg.CloudinaryStorage || pkg.default?.CloudinaryStorage || pkg;
 const { protect } = require('../middleware/auth');
 const router = express.Router();
 
-cloudinary.config({
+
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary, 
   params: {
     folder: 'cyberinf',
     allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp'],
